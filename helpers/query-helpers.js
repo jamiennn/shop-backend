784a5f5d9f75cdc9a9ea739bd3f63e4bf94b11e3
jamiennn <jasper07165@hotmail.com>
2023-09-28 15:18:32 +0800

    feat: add product sorting function

--- helpers/query-helpers.js
+++ helpers/query-helpers.js
@@ -3,10 +3,12 @@ const { getOffset } = require('../helpers/pagination-helpers')
 const getQueryString = (req) => {
   const DEFAUL_LIMIT = 18
   const DEFAULT_MAX_PRICE = 50000
+  const DEFAULT_ORDER = ['createdAt', 'desc']
 
   const keyword = req.query.keyword?.trim()?.toLowerCase() || null
   const priceMax = Number(req.query.priceMax) || DEFAULT_MAX_PRICE
   const priceMin = Number(req.query.priceMin) || 0
+  const order = req.query.order || DEFAULT_ORDER
 
   // 處理複選 categoryId的狀況
   const categoryId = req.query.categoryId
@@ -19,7 +21,7 @@ const getQueryString = (req) => {
 
   const queryText = keyword ? `Product.name LIKE '%${keyword.replace(/['"]+/g, '')}%'` : ''
 
-  return { priceMax, priceMin, categoryIdArray, page, limit, shopId, offset, queryText }
+  return { priceMax, priceMin, order, categoryIdArray, page, limit, shopId, offset, queryText }
 }
 
 module.exports = getQueryString
